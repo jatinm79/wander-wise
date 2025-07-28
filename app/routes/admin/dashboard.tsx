2804b1409854ea0bf7b970626074741ca7c2d5ea
Jatin Mahajan <jatinmahajan2979@gmail.com>
2025-07-28 07:47:25 +0530

    add header section and add title & description in the header

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,9 +1,18 @@
+import { Header } from "components";
 import React from "react";
 
 const Dashboard = () => {
+  const user = { name: "Jatin" };
+
   return (
-  
-  <div>Dashboard Page</div>
-)};
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"} 👋🏻`}
+        description="Track activity, trends and popular destinations in real time"
+      />
+      Dashboard Page Contents
+    </main>
+  );
+};
 
 export default Dashboard;
