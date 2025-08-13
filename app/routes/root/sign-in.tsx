f96c631a591823b8e5dcc702d7bd28e6e071debe
Jatin Mahajan <jatinmahajan2979@gmail.com>
2025-08-13 20:01:52 +0530

    add Google Cloud OAuth2 authentication flow for user and admin Sign-In

--- app/routes/root/sign-in.tsx
+++ app/routes/root/sign-in.tsx
@@ -1,12 +1,20 @@
 import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 import React from "react";
-import { Link } from "react-router";
+import { Link, redirect } from "react-router";
+import { loginWithGoogle } from "~/appwrite/auth";
+import { account } from "~/appwrite/client";
+
+export async function clientLoader() {
+  try {
+    const user = await account.get();
+
+    if (user.$id) return redirect("/");
+  } catch (e) {
+    console.log("Error fetching user", e);
+  }
+}
 
 const SignIn = () => {
-    const handleSignIn = async () => {
-
-    }
-
   return (
     <main className="auth">
       <section className="size-full glassmorphism flex-center px-6">
@@ -28,24 +36,26 @@ const SignIn = () => {
             </h2>
 
             <p className="p-18-regular text-center text-gray-100 !leading-7">
-                Sign in with Google to manage destinations, itineraries and user activity with ease.
+              Sign in with Google to manage destinations, itineraries and user
+              activity with ease.
             </p>
           </article>
 
           <ButtonComponent
-              type="button"
-              iconCss="e-search-icon"
-              className="button-class !h-11 !w-full"
-              onClick={handleSignIn}
-              >
+            type="button"
+            iconCss="e-search-icon"
+            className="button-class !h-11 !w-full"
+            onClick={loginWithGoogle}
+          >
             <img
-               src="/assets/icons/google.svg"
-               className="size-5"
-               alt="google"
-
+              src="/assets/icons/google.svg"
+              className="size-5"
+              alt="google"
             />
 
-            <span className="p-18-semibold text-white">Sign In with Google</span>
+            <span className="p-18-semibold text-white">
+              Sign In with Google
+            </span>
           </ButtonComponent>
         </div>
       </section>
